ae8566b13465b258c33e3282b7053b0e6da9d8c4
Parker Moore <parkrmoore@gmail.com>
2013-01-19 23:46:34 -0500

    Added Update belongs_to :user

--- app/models/update.rb
+++ app/models/update.rb
@@ -2,5 +2,5 @@ class Update < ActiveRecord::Base
   attr_accessible :body, :title, :user_id
   has_attached_file :title_image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
 
-  #belongs_to :user
+  belongs_to :user
 end
